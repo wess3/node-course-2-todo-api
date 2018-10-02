95357360483650a1511730a4fdb4050485c041df
tomas.havelka <tomas.havelka@economia.cz>
2018-10-02 15:58:25 +0200

    feat: DELETE /todos

--- server/server.js
+++ server/server.js
@@ -44,6 +44,20 @@ app.get('/todos/:id', (req, res) => {
   })
 })
 
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findByIdAndRemove(id).then(todo => {
+    todo ? res.send( todo ) : res.status(404).send()
+  }).catch(e => {
+    res.status(400).send()
+  })
+})
+
 app.listen(port, () => console.log(`Started on port ${port}`))
 
 module.exports = { app }
